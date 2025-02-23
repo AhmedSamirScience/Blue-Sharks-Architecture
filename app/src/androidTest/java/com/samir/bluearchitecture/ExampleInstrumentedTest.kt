d01f108d6c98349d9cbe42f3d3f6236eca835ab8
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2025-02-23 23:40:34 +0200

    (1.0-ArchitecturePhase) - (Feature/1.7-Adding-Spotless-Plugin-Code-Formatting) -> remove error related to code inside test class

--- app/src/androidTest/java/com/samir/bluearchitecture/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/samir/bluearchitecture/ExampleInstrumentedTest.kt
@@ -2,6 +2,7 @@ package com.samir.bluearchitecture
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
