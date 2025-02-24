20849646128e52d93f706fafd85ea5847f6f4b0e
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2025-02-25 01:00:14 +0200

    (1.0-ArchitecturePhase) - (Feature/1.9-Adding-Upadte-Dependency-Plugin) -> feat: Integrate Gradle Versions Plugin for automated dependency updates
    - Added `com.github.ben-manes:gradle-versions-plugin:0.51.0` to track outdated dependencies.
    - Introduced `id(plugs.BuildPlugins.UPDATE_DEPS_VERSIONS)` for better version management.
    - Enables automatic checking for newer dependency versions to keep the project up to date.

--- buildSrc/src/main/kotlin/plugs/SharedLibraryGradlePlugin.kt
+++ buildSrc/src/main/kotlin/plugs/SharedLibraryGradlePlugin.kt
@@ -54,7 +54,7 @@ class SharedLibraryGradlePlugin : Plugin<Project> {
         plugins.apply(BuildPlugins.KTLINT)
         plugins.apply(BuildPlugins.SPOTLESS)
         plugins.apply(BuildPlugins.DETEKT)
-        // plugins.apply(BuildPlugins.UPDATE_DEPS_VERSIONS)
+        plugins.apply(BuildPlugins.UPDATE_DEPS_VERSIONS)
         // plugins.apply(BuildPlugins.DOKKA)
     }
 
